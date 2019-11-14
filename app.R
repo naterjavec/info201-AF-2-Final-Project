ebbe6e24367f58990dc02798dd56c37f2c2f64dc
LadyInquist <ethan.hu@live.com>
2019-11-13 20:55:15 -0800

    multi-tab format added

--- app.R
+++ app.R
@@ -9,40 +9,65 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+# Define the first page content; uses `tabPanel()` and `sidebarLayout()`
+# layout functions together (as an example)
+page_one <- tabPanel(
+    "First Page", # label for the tab in the navbar
+    titlePanel("Page 1"), # show with a displayed title
+    
+    # This content uses a sidebar layout
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            textInput(inputId = "username", label = "What is your name?")
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            h3("Primary Content"),
+            p("Plots, data tables, etc. would go here")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define content for the second page
+page_two <- tabPanel(
+    "Second Page" # label for the tab in the navbar
+    # ...more content would go here...
+)
+
+# Define content for the third page
+page_three <- tabPanel(
+    "Third Page" # label for the tab in the navbar
+    # ...more content would go here...
+)
+
+page_four <- tabPanel(
+    "Fourth Page" # label for the tab in the navbar
+    # ...more content would go here...
+)
+
+page_five <- tabPanel(
+    "5th Page" # label for the tab in the navbar
+    # ...more content would go here...
+)
+
+page_six <- tabPanel(
+    "Sixth Page" # label for the tab in the navbar
+    # ...more content would go here...
+)
+# Pass each page to a multi-page layout (`navbarPage`)
+ui <- navbarPage(
+    "My Application", # application title
+    page_one,         # include the first page content
+    page_two,         # include the second page content
+    page_three,
+    page_four, 
+    page_five,
+    page_six          # include the third page content
+)
+
+# Define server logic 
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
