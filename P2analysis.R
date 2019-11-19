71d5c92a0fa9c51fcb13e128ef820dac9e9f4fdd
naterjavec <nerjavec@uw.edu>
2019-11-18 20:44:44 -0800

    Added scatterplot option

--- P2analysis.R
+++ P2analysis.R
@@ -1,7 +1,6 @@
 #P2 Analysis for Shiny App
 
 
-<<<<<<< HEAD
 #Download Libraries
 library(dplyr)
 library(ggplot2)
@@ -17,6 +16,8 @@ global_temp <- read.csv("data/GlobalLandTemperaturesByMajorCity.csv")
 
 cities_temp <- unique(global_temp$City)
 cities_food <- unique(food_prices$adm1_name)
+
+View(cities_temp)
 
 
 #Select from list of cities in both datasets
@@ -63,12 +64,14 @@ merge_data <- function(df1, df2){
 }
 
 
-#This is awful
-p <- plot_ly(Lima_data, x = ~AverageTemperature, y = ~mp_price, type = 'scatter', mode = 'lines')
 
-print(p)
-
-#Thoughts?
+#Fix best fit line, just connecting top - not regression
+scatter_plot <- function(df){
+  return(plot_ly(df, x = ~date, y = ~AverageTemperature, name = 'Average Temperature',
+                 type = 'scatter') %>%
+           add_trace(y = ~mp_price, name = 'Maket Price') %>%
+           add_lines(x = ~date, y = fitted(~mp_price)))
+}
 
 line_plot <- function(df){
   return(plot_ly(df, x = ~date, y = ~AverageTemperature, name = 'Average Temperature',
@@ -92,5 +95,16 @@ print(Lima_plot)
 #Delhi and mustard oil
 Delhi_plot <- data_and_plot("Delhi", "Oil (mustard)")
 print(Delhi_plot)
-=======
->>>>>>> 7c3c2f70b3114a129246406fe0725d5b46ef26c8
+
+
+#Delhi and wheat
+Delhi_wheat <- data_and_plot("Delhi", "Wheat")
+print(Delhi_wheat)
+
+
+#Unique Delhi Foods
+Delhi_foods <- food_prices %>%
+  filter(adm1_name == "Delhi")
+
+Delhi_foods_u <- unique(Delhi_foods$cm_name)
+View(Delhi_foods_u)
