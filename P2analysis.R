aa5196dbce5e1b3cbca62025c8da9d04993680b0
naterjavec <nerjavec@uw.edu>
2019-11-24 10:56:55 -0800

    added percentages to data

--- P2analysis.R
+++ P2analysis.R
@@ -17,12 +17,10 @@ global_temp <- read.csv("data/GlobalLandTemperaturesByMajorCity.csv")
 cities_temp <- unique(global_temp$City)
 cities_food <- unique(food_prices$adm1_name)
 
-View(cities_temp)
 
 
 #Select from list of cities in both datasets
 cities_list <- intersect(cities_food, cities_temp)
-View(cities_list)
 
 
 
@@ -43,6 +41,7 @@ city_food_data <- function(city, food){
   return(food_data)
 }
 
+
 #food data starts in 2006 and global temp data begins in 1881
 #To make sure we show data that we have collected for both years
 #This function includes filtering years > 2006 then selects
@@ -57,15 +56,18 @@ city_temp_data <- function(city){
   return(temp)
 }
 
-
 #Method to merge the two data sets into one for plot
 merge_data <- function(df1, df2){
   return(merge(df1, df2, by = "date"))
 }
 
 
+#Merge isnt working - Ethan will fix dates
+example <- merge_data(city_food_data("Delhi", "Wheat"), city_temp_data("Delhi"))
+View(example)
 
-#Fix best fit line, just connecting top - not regression
+
+#Fix best fit line, rn just connecting top - not regression
 scatter_plot <- function(df){
   return(plot_ly(df, x = ~date, y = ~AverageTemperature, name = 'Average Temperature',
                  type = 'scatter') %>%
@@ -73,6 +75,8 @@ scatter_plot <- function(df){
            add_lines(x = ~date, y = fitted(~mp_price)))
 }
 
+
+#working fine but not preferable
 line_plot <- function(df){
   return(plot_ly(df, x = ~date, y = ~AverageTemperature, name = 'Average Temperature',
                  type = 'scatter', mode = 'lines',
@@ -81,24 +85,26 @@ line_plot <- function(df){
                      line = list(color = 'rgb(22, 96, 167)', width = 4)))
 }
 
+
+#takes in city and food and returns information as plot
 data_and_plot <- function(city, food){
   city_data <- merge_data(city_food_data(city, food), city_temp_data(city))
   return(line_plot(city_data))
 }
 
+#Tests and Plots
 
-#Lima And Maize tests
+
+#Lima And Maize plot
 Lima_plot <- data_and_plot("Lima", "Maize (local)")
 print(Lima_plot)
 
 
-#Delhi and mustard oil
+#Delhi and mustard oil plot
 Delhi_plot <- data_and_plot("Delhi", "Oil (mustard)")
 print(Delhi_plot)
 
-
-
-#Delhi and wheat
+#Delhi and wheat plot
 Delhi_wheat <- data_and_plot("Delhi", "Wheat")
 print(Delhi_wheat)
 
@@ -110,14 +116,62 @@ Delhi_foods <- food_prices %>%
 Delhi_foods_u <- unique(Delhi_foods$cm_name)
 View(Delhi_foods_u)
 
+#---------------------- adding percent changes--------------------------
 
+temp_w_percent <- global_temp %>%
+  mutate(month = substring(dt, 6, 7),
+         year = as.numeric(substring(dt, 1, 4))) %>%
+  filter(month == "10") %>%
+  group_by(City) %>%
+  filter(year == 2006 | year == 2012) %>%
+  mutate(change = AverageTemperature - lag(AverageTemperature, default = AverageTemperature[1])) %>%
+  mutate(percent_change = change / lag(AverageTemperature, default = AverageTemperature[1]) * 100) %>%
+  filter(percent_change != 0) %>%
+  select(City, Country, percent_change)
+
+
+food_change<- food_prices %>%
+  filter(mp_month == 10) %>%
+  group_by(adm1_name, cm_name) %>%
+  filter(mp_year == max(mp_year) | mp_year == min(mp_year)) %>%
+  mutate(price_change = mp_price - lag(mp_price, default = mp_price[1]),
+         City = adm1_name) %>%
+  mutate(food_percent = price_change / lag(mp_price, default = mp_price[1]) * 100) %>%
+  filter(price_change != 0) %>%
+  select(City, adm0_name, adm1_name, cm_name, food_percent)
+
+
+percent_change <- merge(food_change, temp_w_percent, by = "City")
+
+  
+ggplot(percent_change, aes(fill = food_percent, y = percent_change, x = City)) + 
+  geom_bar(position="stack", stat="identity")
+
+
+#Function to create bar plot based on specific foods
+#Returns barchart with any city that has data about the food parameter entered 
+create_bar_chart <- function(food){
+  specific_data <- percent_change %>%
+    filter(cm_name == food)
+  return(ggplot(specific_data, aes(fill = food_percent, y = percent_change, x = City)) + 
+           geom_bar(position="stack", stat="identity"))
+}
+
+
+print(create_bar_chart("Wheat flour"))
+
+#Wheat, Rice, Sugar, Rice (local), Lentils, Maize (local), Bread, Wheat flour
 
 
 # App server stuff - have it do it for every 
 # have a couple cities, have the options for mutliple different foods for each city
 # two different y axis? one with Degrees Celsius and one with market price / KG
+#Sierra ^^
 
 # percent change for both market price of food and temp
 # Do this based on the same month of 2006 to 2013 (i.e. March)
+
+# heat map 
+# using original data sets of both with longitude and lat
 
 
