a1746bbd2cb5e52e1408e9003a09447a53c464eb
LadyInquist <ethan.hu@live.com>
2019-12-02 20:30:44 -0800

    gamebreaking change

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library("shiny")
 library("plotly")
+library("shinythemes")
 source("P2analysis.R")
 library("ggplot2")
 
@@ -75,7 +76,7 @@ page_two <- tabPanel(
                     companies to wedge a gap between the upper class and
                     the middle and lower class, creating more divide than 
                     our nation already faces."),
-                 # plotOutput(create_bar_chart())
+                 # plotOutput(create_bar_chart(Wheat))
               )
             )
 )
@@ -116,8 +117,8 @@ page_four <- tabPanel(
                     selectInput("city_select", "Choose a City:",
                                 choices = cities_list)),
                 mainPanel(
-                    #data_and_plot("Delhi", "Wheat")
-                    plotOutput("cityPlot")
+                    
+                    plotOutput("plot")
                 )
             )
 )
@@ -135,26 +136,33 @@ page_five <- tabPanel(
                     and in-page formatting, connecting the
                       server to the user interface, and inputting
                       visualizations into the ui."),
-                    p("Ethan:"),
+                    p("Ethan: I worked on implementing the Techichal
+                      Report and Project proposal, along with the hosting
+                      of the Shiny App. I also had a role in troubleshooting
+                      GitHub branches and analysis files and functions."),
                     p("Tyler:"),
                     h3("Data Sets:"),
                     h5(uiOutput("tab")),
                     h5(uiOutput("tab2"))
                 ),
                 mainPanel(
-                    p("In the project, we found ourselves wanting to
+                    h3("Challenges"),
+                    p("In this project, we found ourselves wanting to
                       compare many different types of crops in order 
                       to get a better picture of the effects of 
                       climate change, however, were unable to do
                       so because of time constraints. In addition,
                       comparing different times of the year, as 
                       opposed to solely June 1st of each year, would
-                      have also provided us with more credibility.
-                      At the same time, we believe that our
+                      have also provided us with more credibility."),
+                    h3("Usage"),
+                     p(" At the same time, we believe that our
                       consistency among variables such as time
                       of year and crop are what makes the project
                       reliable and appeals to the ease of
-                      readability for the user. From this, 
+                      readability for the user."),
+                    h3("Learning Opprotunities"),
+                      p("From this, 
                       we learned how to apply Shiny to real-world 
                       scenarios and get results. From here, one
                       may want to pursue using a different data
@@ -169,6 +177,7 @@ page_five <- tabPanel(
 
 
 ui <- navbarPage(
+            theme = shinytheme("united"),
             "The Effect of Climate Change on Food Prices",
             page_one,
             page_two,
@@ -179,15 +188,16 @@ ui <- navbarPage(
         
 server <- function(input, output) {
 
-       # output$cityPlot <- renderPlot({
-        #    data_and_plot(city_temp_data(input$City), city_food_data(input$cm_name))
-        #})
+       output$cityPlot <- renderPlot({
+          data_and_plot(city_temp_data(input$City), 
+                        city_food_data(input$cm_name))
+        })
     
-    #output$plot <- renderPlotly(data_and_plot())
-        #plot_ly()
-        #datasetInput <- eventReactive(input$update,
-        #    switch(input$data_and_plot))
-        #output$view <- renderPlotly
+    output$plot <- renderPlotly(data_and_plot())
+        plot_ly()
+        datasetInput <- eventReactive(input$update,
+            switch(input$data_and_plot))
+        output$view <- renderPlotly
 }
         
 shinyApp(ui = ui, server = server)
